f29d540b8b9ec686dd8008115a85f95504bcd666
khushChopra <khush.chopra.cse16@itbhu.ac.in>
2019-08-24 18:39:04 +0530

    test php on ec2

--- test.php
+++ test.php
@@ -1,3 +1,18 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+$servername = "database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "adminpass";
+$database = "mayo";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'),true);
+
 echo "Hey";
 ?>
--- user.php
+++ user.php
@@ -1,9 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-$connectionInfo = array("UID" => "admin", "pwd" => "adminpass", "Database" => "mayo", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com,3306";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+$servername = "database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "adminpass";
+$database = "mayo";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'),true);
 
