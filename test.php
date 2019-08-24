af39f7da28bbff881299b730a971b96f8d96d27e
khushChopra <khush.chopra.cse16@itbhu.ac.in>
2019-08-24 19:16:32 +0530

    test php on ec2

--- test.php
+++ test.php
@@ -1,12 +1,17 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-
-$servername = "database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com";
+$servername = "database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com:3306";
 $username = "admin";
 $password = "adminpass";
 $database = "mayo";
+
+function message_and_code($message, $code){
+    $temp = array();
+    $temp["message"] = $message;
+    http_response_code($code);
+    echo json_encode($temp);
+}
 
 
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -15,9 +20,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'),true);
 
 if(!$conn){
-    echo "didnt connect";
+    message_and_code("didnt connect",200);
 }
 else{
-    echo "connected";
+    message_and_code("connected",200);
 }
 ?>
