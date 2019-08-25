5c7c60af36a37c6c1e212dcb883690e12d0f5bb2
khushChopra <khush.chopra.cse16@itbhu.ac.in>
2019-08-25 06:09:43 +0530

    service added

---
+++ service.php
@@ -0,0 +1,67 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "database-mayo1.cusbnuvbxuoj.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "adminpass";
+$database = "mayo";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'),true);
+
+
+
+$result = array();
+function message_and_code($message, $code){
+    $temp = array();
+    $temp["message"] = $message;
+    http_response_code($code);
+    echo json_encode($temp);
+}
+switch ($method) {
+    case 'GET':
+
+    Create table govt (
+    serviceID  int AUTO_INCREMENT PRIMARY KEY,
+    contact varchar(256),
+    city varchar(256),
+    title varchar(256),
+    body varchar(256)
+)
+
+
+        $tsql1 = "select * from govt where city='".$_GET['city']."'";
+        $getResults= mysqli_query($conn, $tsql1);
+
+        $count = 0;
+        while($row = mysqli_fetch_array($getResults)){
+            $result['service'][$count]['body'] = $row['body'];
+            $result['service'][$count]['title'] = $row['title'];
+            $result['service'][$count]['contact'] = $row['contact'];
+            $result['service'][$count]['serviceID'] = $row['courseID'];
+            $result['service'][$count]['city'] = $row['city'];
+            $count = $count + 1;
+        }
+        $result["count"] = $count;
+        echo json_encode($result);
+        break;
+    case 'POST':
+        $tsql1 = "insert into govt (city, contact,body,title) VALUES ('".$input['city']."','".$input['contact']."','".$input['body']."','".$input['title']."')";
+        $insertReview = mysqli_query($conn, $tsql1);
+        // check for server error
+        if($insertReview==FALSE){
+            message_and_code("Server error",500);
+            break;
+        }
+        else{
+            message_and_code("Success",200);
+            break;
+        }
+        break;
+}
+mysqli_close($conn);
+?>
